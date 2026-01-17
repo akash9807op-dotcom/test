9e7e4660ac0759a0f6b7fcaa4c0608ca40f0eca4
akash9807op-dotcom <akash9807op@gmail.com>
2026-01-17 18:38:04 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
-from fastapi import FastAPI,Request
-from fastapi.middleware.cors import CORSMiddleware
-from . import models
-from .database import engine
-from .routers import posts,users,login,vote
 import os
-from dotenv import load_dotenv
-load_dotenv()
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+
+# Internal imports
+from . import models
+from .database import engine
+from .routers import posts, users, login, vote
+
+load_dotenv()
+
 models.Base.metadata.create_all(bind=engine)
+
+# --- FIX START: Create Absolute Paths ---
+# Get the location of THIS file (main.py)
 current_dir = os.path.dirname(os.path.abspath(__file__))
+# Create full paths to static and templates
+static_path = os.path.join(current_dir, "static")
+templates_path = os.path.join(current_dir, "templates")
+# --- FIX END ---
 
-app=FastAPI()# app is instance of fastapi application 
+app = FastAPI()
 
-origins=["*"]
+origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -22,110 +33,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+# Use the absolute paths we created above
+app.mount("/static", StaticFiles(directory=static_path), name="static")
+templates = Jinja2Templates(directory=templates_path)
 
 @app.get('/')
-def root(request:Request):
-    return templates.TemplateResponse("/templates/home.html", {"request": request})
+def root(request: Request):
+    # Just use "home.html". NOT "/templates/home.html"
+    return templates.TemplateResponse("home.html", {"request": request})
+
 app.include_router(vote.router)
 app.include_router(login.router)
 app.include_router(posts.router)
 app.include_router(users.router)
-
-
-# git command to commit and push into git
-# git add --all
-# git commit -m "added new feature"   contian the commit name
-# git push origin main      to push all in orgin main
-
-
-
-    
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Post(BaseModel):
-#     title:str
-#     content:str
-#     published:bool=True
-# @app.get('/')
-# def home():
-#     return "hellllllooooo"
-# @app.post('/h')
-# def x(new_post:Post):
-# # new_post is a Pydantic model created from the request body.
-# # It validates the incoming data and converts it to a Python object.
-# # Use new_post.dict() to access the data as a dictionary.
-#     if new_post.published==False:
-#         return f" Hey  {new_post.title}\n why you dont want to published"
-#     return f"Hey {new_post.title} {new_post.content} why did you published this------"
-# from fastapi import FastAPI
-# app = FastAPI()
-
-
-
-# @app.post("/items/")
-# def read_item():
-#     return {"item_id":"34"}
-# @app.get("/items/")
-# def read_item():
-#     return {"item_id":"35"}
-
-# # The Database
-# my_posts = [{"id": 1, "title": "Top post", "content": "check out this content"}]
-
-# @app.get("/posts/{id}")
-# def get_post(id: int):
-#     # Logic to find the post
-#     for post in my_posts:
-#         if post["id"] == id:
-#             return post
-    
-#     # If the loop finishes and we didn't find the ID...
-#     return {"message": "Post not found"}
-
-
-
