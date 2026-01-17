1e6ae32f1e5ce2c7d4b387ea678df1b238691f5f
akash9807op-dotcom <akash9807op@gmail.com>
2026-01-17 20:46:03 +0530

    Update oauth2.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -9,7 +9,7 @@ load_dotenv()
 Oauth2=OAuth2PasswordBearer(tokenUrl='login')
 def create_access_token(data:dict):
     to_encode=data.copy()
-    expire=datetime.utcnow() + timedelta(minutes=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    expire=datetime.utcnow() + timedelta(minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
     to_encode.update({"exp":expire})
     access_token=jwt.encode(to_encode,{os.getenv('SECRET_KEY')},algorithm=os.getenv('ALGORITHM'))
     return access_token
@@ -28,4 +28,5 @@ def verify(token:str,credential_Exception):
     return data
 def get_current_user(token:str=Depends(Oauth2)):
     credential_exception=HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="InValid Credential",headers={"WWW-Authenticate":"Bearer"})
+
     return verify(token,credential_exception)
